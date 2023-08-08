84b3142b96fe5224b5d4240e58da8c6588a822fa
MenesK2M <117518027+MenesK2M@users.noreply.github.com>
2023-08-07 23:44:24 -0400

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,1 +1,11 @@
-
+def call () {
+  container('jnlp'){
+               git branch: 'dev', url: 'https://github.com/MenesK2M/Java_project.git'
+               
+               script{
+                   COMMIT_ID = sh (
+                       script: "git log -n 1 --pretty=format:'%H' | cut -c1-5",
+                       returnStdout: true)
+               }
+           }
+}
