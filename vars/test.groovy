d78c82db229c2a736bb704d57d7934e72fc84bd9
MenesK2M <117518027+MenesK2M@users.noreply.github.com>
2023-08-08 00:57:52 -0400

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,5 +1,3 @@
 def call() {
-  container('jnlp'){
-    git branch: 'main', url: 'https://github.com/MenesK2M/Java_project.git'
-  }
+  git branch: 'main', url: 'https://github.com/MenesK2M/Java_project.git'
 }
