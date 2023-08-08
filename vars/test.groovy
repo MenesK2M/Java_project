414dd8f2e8138473098d20d667892e159b50b541
MenesK2M <117518027+MenesK2M@users.noreply.github.com>
2023-08-08 00:41:42 -0400

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,10 +1,5 @@
 def call() {
   container('jnlp'){
     git branch: 'main', url: 'https://github.com/MenesK2M/Java_project.git'
-    script{
-      COMMIT_ID = sh (
-        script: "git log -n 1 --pretty=format:'%H' | cut -c1-5",
-        returnStdout: true)
-    }
   }
 }
